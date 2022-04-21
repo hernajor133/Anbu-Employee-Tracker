44b9477c5408a12d996f9a148564549dffa99cc4
Jordi <jordihernandez133@gmail.com>
2022-04-21 14:25:25 -0700

    romoved some files and installed new dependancies

--- db/query.sql
+++
@@ -1,1 +0,0 @@
-USE company_db;
--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,19 @@
+-- Drops database in case of TEST --
 DROP DATABASE IF EXISTS company_db;
+-- Creates the "company_db" database --
 CREATE DATABASE company_db;
 
+-- Selects the "company_db database" --
 USE company_db;
 
+-- Creates the table "departments" within company_db --
 CREATE TABLE departments (
+      -- Creates a numeric column called "id" which will automatically increment its default value  --
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
 
+-- Creates the table "roles" within company_db --
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     job_title VARCHAR(30),
@@ -18,6 +24,7 @@ CREATE TABLE roles (
     ON DELETE SET NULL
 );
 
+-- Creates the table "employees" within company_db --
 CREATE TABLE employees (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
