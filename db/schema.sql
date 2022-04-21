dece6b3800949f5ab536acfe77cc4e1c72b472a2
Jordi <jordihernandez133@gmail.com>
2022-04-21 14:27:43 -0700

    updated schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,44 +1,38 @@
--- Drops database in case of TEST --
-DROP DATABASE IF EXISTS company_db;
--- Creates the "company_db" database --
-CREATE DATABASE company_db;
-
--- Selects the "company_db database" --
-USE company_db;
-
--- Creates the table "departments" within company_db --
-CREATE TABLE departments (
-      -- Creates a numeric column called "id" which will automatically increment its default value  --
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30) NOT NULL
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+USE employees;
+DROP TABLE IF EXISTS manager;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employee;
+CREATE TABLE manager (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    PRIMARY KEY (id)
 );
-
--- Creates the table "roles" within company_db --
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
 CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    job_title VARCHAR(30),
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
     department_id INT,
-    salary INT,
-    FOREIGN KEY (department_id)
-    REFERENCES departments(id)
-    ON DELETE SET NULL
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
-
--- Creates the table "employees" within company_db --
-CREATE TABLE employees (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     role_id INT,
-    department_id INT,
     manager_id INT,
-    FOREIGN KEY (manager_id)
-    REFERENCES employees(id)
-    ON DELETE SET NULL,
-    FOREIGN KEY (role_id)
-    REFERENCES roles(id)
-    ON DELETE SET NULL,
-    FOREIGN KEY (department_id)
-    REFERENCES departments(id)
-    ON DELETE SET NULL
+    manager_confirm BOOLEAN,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES roles(id) ON DELETE
+    SET NULL
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,17 +1,33 @@
-INSERT INTO departments (department_name)
-VALUES ("Software Development"),
-       ("Finance"),
-       ("Legal"),
-       ("Sales");
-
-INSERT INTO roles (job_title, department_id, salary)
-VALUES ("Head of Marketing", 4, 120000),
-       ("Accountant", 2, 100000),
-       ("Software Engineer", 1, 90000),
-       ("Lawyer", 3, 110000);
-
-INSERT INTO employees (first_name, last_name, role_id, department_id, manager_id)
-VALUES ("Jose", "Mañuel", 1, 3, NULL),
-       ("Cleo", "Shmidt", 2, 2, 1),
-       ("Hong", "Mai", 3, 4, 1),
-       ("Gary", "Zhang", 4, 1, 1);
+INSERT INTO department (name)
+VALUES ('Legal'),
+    ('Sales'),
+    ('Engineering');
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Legal Team Lead', 250000, 1),
+    ('Lawyer', 170000, 1),
+    ('Sales Lead', 100000, 2),
+    ('Salesperson', 70000, 2),
+    ('Lead Engineer', 150000, 3),
+    ('Engineer', 120000, 3);
+INSERT INTO employee (
+        first_name,
+        last_name,
+        role_id,
+        manager_id,
+        manager_confirm
+    )
+VALUES ('Jerry', 'Underwood', 1, null, true),
+    ('Bob', 'Sheldon', 2, 1, false),
+    ('Jason', 'Mendoza', 2, 1, false),
+    ('Alex', 'Jackson', 3, null, true),
+    ('Peter', 'Makah', 4, 2, false),
+    ('Suzie', 'Alisson', 4, 2, false),
+    ('John', 'Winger', 4, 2, false),
+    ('Pete', 'McFall', 5, null, true),
+    ('Alexis', 'Caper', 6, 3, false),
+    ('Mason', 'Jacobson', 6, 3, false);
+INSERT INTO manager (first_name, last_name)
+SELECT first_name,
+    last_name
+FROM employee
+WHERE manager_confirm = 1;
