1982eeb33849955ff02fa8eee3848d545a3df5c2
Jordi <jordihernandez133@gmail.com>
2022-04-22 20:04:38 -0700

    final edits

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,10 +5,10 @@ VALUES ("Software Development"),
        ("Sales");
 
 INSERT INTO roles (job_title, department_id, salary)
-VALUES ("Marketing", 4, 120000),
-       ("Accountant", 2, 100000),
+VALUES ("Marketing", 4, 100000),
+       ("Accountant", 2, 90000),
        ("Software Engineer", 1, 90000),
-       ("Lawyer", 3, 110000);
+       ("Lawyer", 3, 120000);
 
 INSERT INTO employees (first_name, last_name, role_id, department_id, manager_id)
 VALUES ("Jose", "Herrera", 1, 3, NULL),
